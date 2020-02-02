00380fec52a7b4c114ef86e57b359bb5aaf92659
mrnobody2002 <filipemelo032@gmail.com>
2020-02-01 22:54:58 -0300

    nav collapse menu options changed

--- src/pages/components/Header/index.js
+++ src/pages/components/Header/index.js
@@ -5,15 +5,13 @@ import './styles.css'
 export default function Header(){
     return (
         <Navbar sticky="top"  bg="light" collapseOnSelect expand="lg" className="menu">
-            <Navbar.Brand href="#" className="brand"><h1>Gerson Vaz <strong>Fotografia</strong></h1></Navbar.Brand>
+            <Navbar.Brand href="#" className="brand">Gerson Vaz <strong>Fotografia</strong></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#ensaios">Ensaios Externos</Nav.Link>
-                    <Nav.Link href="#eventos">Eventos</Nav.Link>
+                    <Nav.Link href="#eventos">Outros Eventos</Nav.Link>
                     <Nav.Link href="#wallpapers">Wallpapers</Nav.Link>
-                    <Nav.Link href="#casamento">Fotografia de Casamento</Nav.Link>
-                    <Nav.Link href="#outros">Outros pacotes</Nav.Link>
                 </Nav>
                 <Form inline>
                     <Button variant="outline-primary">Agende agora</Button>
