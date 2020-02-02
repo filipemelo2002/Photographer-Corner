13134569ea471809cac5e90f5b3b940efe017aa6
mrnobody2002 <filipemelo032@gmail.com>
2020-02-02 10:18:13 -0300

    cards section

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import './styles.css'
+
+import img1 from '../../assets/photo-camera.png'
+import img2 from '../../assets/focus.png'
+import img3 from '../../assets/video-editing.png'
+import img4 from '../../assets/cable.png'
+import img5 from '../../assets/panoramic-view.png'
 export default function Home(){
     return (
         <div className="content">
@@ -10,6 +16,28 @@ export default function Home(){
                 <p>
                 Olá! Meu nome é Gerson Vaz e trabalho com fotografia desde 2019. Sou formado em Fotografia Digital pela GINEAD e em Photoshop pela Prime Cursos. Desafios são minha motivação e fotografar é um sonho realizado. E aí, vai um click?
                 </p>
+            </div>
+            <div className="floatingCards">
+                <div className="floatCard">
+                    <img src={img1} alt="icon-camera"/>
+                    <p>Lorem Ipsum</p>
+                </div>
+                <div className="floatCard">
+                    <img src={img2} alt="icon-camera"/>
+                    <p>Lorem Ipsum</p>
+                </div>
+                <div className="floatCard">
+                    <img src={img3} alt="icon-camera"/>
+                    <p>Lorem Ipsum</p>
+                </div>
+                <div className="floatCard">
+                    <img src={img4} alt="icon-camera"/>
+                    <p>Lorem Ipsum</p>
+                </div>
+                <div className="floatCard">
+                    <img src={img5} alt="icon-camera"/>
+                    <p>Lorem Ipsum</p>
+                </div>
             </div>
         </div>
         )
