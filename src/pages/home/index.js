fc4017a54de9bad163a9f4ed9a0c96fa786ca7c9
mrnobody2002 <filipemelo032@gmail.com>
2020-02-01 23:44:49 -0300

    About me  section

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 import './styles.css'
 export default function Home(){
     return (
-        <div className="container">
-            
+        <div className="content">
+            <div className="aboutMe">
+                <div>
+                    Sobre mim
+                </div>
+                <p>
+                Olá! Meu nome é Gerson Vaz e trabalho com fotografia desde 2019. Sou formado em Fotografia Digital pela GINEAD e em Photoshop pela Prime Cursos. Desafios são minha motivação e fotografar é um sonho realizado. E aí, vai um click?
+                </p>
+            </div>
         </div>
         )
 }
