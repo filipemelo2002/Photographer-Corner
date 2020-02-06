ad99c5499c8f7e78b3e602b62144ba7181913dbb
mrnobody2002 <filipemelo032@gmail.com>
2020-02-06 17:29:46 -0300

    Home screen design illustration

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,23 +3,53 @@ import React from 'react'
 import './styles.css'
 import Carousel from './carousel'
 import avatar from './img/avatar.png'
+import illustration from './img/illustration.png'
 
 import {FaCamera} from 'react-icons/fa'
 import {Button} from 'react-bootstrap'
+
 export default function Home(){
 
 return (
     <div className="content">
         <Carousel/>
-        <div className="aboutMe">
-            <img src={avatar} alt="about-me-avatar"/>
-            <section>
+        <div className="aboutSection">
+            <div className="aboutMe">
+                <img src={avatar} alt="about-me-avatar"/>
                 <h1>Gerson Vaz</h1>
-                <p>Olá! Meu nome é Gerson Vaz e trabalho com fotografia desde 2019. Sou formado em Fotografia Digital pela GINEAD e em Photoshop pela Prime Cursos. Desafios são minha motivação e fotografar é um sonho realizado. E aí, vai um click?</p>
-                <Button variant="outline-secondary" className="btn">Agende agora <FaCamera /></Button>
-            </section>
-            
+                <section>
+                    <p>
+                        Fala galera, meu nome é Gerson, eu tenho 23 anos e trabalho com fotografia desde 2019. Sempre fui apaixonado por artes. Música, desenhos, cinema essas coisas sempre me encantaram e encheram os meus olhos. Não é diferente com a fotografia.
+                    </p>
+                    <p>
+                        Ao fotografar eu sinto alegria em saber que estou fazendo o que eu gosto. Sempre fui grato a Deus por tudo o que Ele fez na minha vida e também sou grato pelo imenso prazer de tirar fotos.
+                    </p>
+                    <p>
+                        Sou formado em Fotografia Digital pelo instituto GINEAD e em Photoshop pela Prime Cursos. Especializado em retratos, porque eu acredito na combinação ideal entre a fotografia e as pessoas. Representar cada olhar, cada expressão, cada sentimento faz parte do meu trabalho. Eu acredito também que a fotografia pra mim é o trabalho que exemplifica a frase de Confúcio: 
+                        <blockquote className="blockquote mb-0">
+                            <p>
+                                {' '}“Escolha um trabalho que você ama e você nunca terá que trabalhar um dia sequer na vida.”{' '}
+                            </p>
+                            <footer className="blockquote-footer">
+                                Confúcio
+                            </footer>
+                        </blockquote>
+                    </p>
+                    <p>
+                        Desafios me motivam e não desisto tão fácil dos meus objetivos. E sobre fotografia... nunca é só um click, sempre conseguimos experiências novas e incomparáveis.
+                    </p>
+                    <p>
+                        E aí? Me conta as tuas ideias que vou fazer de tudo para transformá-las em recordações incríveis. Vai um click?
+                    </p>
+                    <Button variant="outline-secondary" className="btn">Agende agora <FaCamera /></Button>
+                </section>
+                
+            </div>
+            <div className="illustration">
+                <img src={illustration} id="illustration" alt="about-me-avatar"/>
+            </div>
         </div>
+        
     </div>  
     )
 }
