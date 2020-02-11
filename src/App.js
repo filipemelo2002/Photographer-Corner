255940d61f2b554826a5feae69b12865ce1dc5cc
mrnobody2002 <filipemelo032@gmail.com>
2020-02-10 21:32:48 -0300

    Footer loaded within App start

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './Routes'
 import Header from './pages/components/Header'
 
+import Footer from './pages/components/Contact'
 import './App.css'
 function App() {
   return( 
     <>
       <Header />
       <Routes />
+      <Footer />
     </>
 
   )
