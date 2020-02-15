b709bd883f5a332fd7b84f0b9d0802cac4545ffa
mrnobody2002 <filipemelo032@gmail.com>
2020-02-15 11:43:17 -0300

    Galleries styles

--- src/pages/eventos/index.js
+++ src/pages/eventos/index.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
+import Spinner from 'react-bootstrap/Spinner'
+import GalleryList from '../components/GalleryList'
+
+import './styles.css'
 
 export default function Eventos(){
-    return <h1>Eventos</h1>
+    const [display, setDisplay] = useState('block')
+    const [imgs, setImgs] = useState([])
+
+
+    useEffect(()=>{
+        async function loadImages(){
+            function importAll(r) {
+                return r.keys().map(r);
+            }
+            const images = importAll(require.context('./img', true, /\.(png|jpe?g|svg|jpg)$/))
+            for(var i=0; i<19; i++){
+                images.push(images[0])
+            }
+            if(images){
+                setImgs(images)
+                setDisplay('none')
+            }
+        }   
+        loadImages()
+    }, [])
+
+    return (
+        <div className="galeryContent">
+            <h1>Eventos</h1>
+            <Spinner animation="grow" className="loadingIndicator" style={{display: display}}/>
+            <GalleryList images={imgs}/>
+        </div>
+    )
 }
