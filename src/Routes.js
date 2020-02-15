736bd066dcbb1df5998ac6f42b7d802aefcd8779
mrnobody2002 <filipemelo032@gmail.com>
2020-02-14 23:17:01 -0300

    Empty pages routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,18 @@ import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Home from './pages/home'
+import Ensaios from './pages/ensaios'
+import Eventos from './pages/eventos'
+import Wallpapers from './pages/wallpapers'
+
 export default function Routes (){
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Home}/>  
+                <Route exact path="/" component={Home}/> 
+                <Route exact path="/ensaios" component={Ensaios}/> 
+                <Route exact path="/eventos" component={Eventos}/>
+                <Route exact path="/wallpapers" component={Wallpapers}/> 
             </Switch>
         </BrowserRouter>
     )
