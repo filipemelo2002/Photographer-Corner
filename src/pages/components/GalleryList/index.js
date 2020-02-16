8922c427cb9171a4f649a9655b97273a41d09e5f
mrnobody2002 <filipemelo032@gmail.com>
2020-02-16 10:32:11 -0300

    Watermark presets

--- src/pages/components/GalleryList/index.js
+++ src/pages/components/GalleryList/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import './styles.css'
+import watermark from './watermark.png'
 export default function GalleryList({images}){
 
     return (
         <div className="gallery">
             {
                 images.map(img=>(
-                        <div className="imgFrame" key={img}>
-                            <img src={img} alt="images" style={{width:'350px'}} />
+                        <div className="imgFrame" key={img} style={{backgroundImage:`url(${img})`, width:'350px', backgroundPosition:'center', backgroundSize:'350px'}}>
+                            <img src={watermark} alt="images" style={{width:'350px'}} />
                         </div>
                     )
                 )
