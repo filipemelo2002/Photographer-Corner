6d7a76b44e05f44c3649337dcc7806ce06203560
mrnobody2002 <filipemelo032@gmail.com>
2020-02-17 16:44:02 -0300

    Admin actions

--- src/pages/components/GalleryList/index.js
+++ src/pages/components/GalleryList/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import {FaTrashAlt} from 'react-icons/fa'
+import {AiFillFolderAdd}from 'react-icons/ai'
 import './styles.css'
 import watermark from './watermark.png'
 export default function GalleryList({images}){
@@ -8,12 +10,18 @@ export default function GalleryList({images}){
         <div className="gallery">
             {
                 images.map(img=>(
-                        <div className="imgFrame" key={img} style={{backgroundImage:`url(${img})`, width:'350px', backgroundPosition:'center', backgroundSize:'350px'}}>
+                        <div className="imgFrame" key={img} style={{backgroundImage:`url(${img})`}}>
                             <img src={watermark} alt="images" style={{width:'350px'}} />
+                            <div className="adminRemove">
+                                <FaTrashAlt className="deleteIcon"/>
+                            </div>
                         </div>
                     )
                 )
             }
+                <div className="addFrame">
+                    <AiFillFolderAdd className="addIcon"/>
+                </div>
         </div>
     )
 
