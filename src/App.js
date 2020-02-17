2c8d7698b8826d50777c61e92600198888616542
mrnobody2002 <filipemelo032@gmail.com>
2020-02-17 16:43:35 -0300

    Removed Header and Footer component from main

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './Routes'
-import Header from './pages/components/Header'
 
-import Footer from './pages/components/Contact'
 import './App.css'
 function App() {
-  return( 
-    <>
-      <Header />
+  return(
       <Routes />
-      <Footer />
-    </>
-
   )
 }
 
