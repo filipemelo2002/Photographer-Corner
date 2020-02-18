ff7232a44f960697b8a9f6ba9dd151975c6026e8
mrnobody2002 <filipemelo032@gmail.com>
2020-02-18 19:15:03 -0300

    Header sticky to top property removed

--- src/pages/components/Header/index.js
+++ src/pages/components/Header/index.js
@@ -4,7 +4,7 @@ import {Navbar, Nav} from 'react-bootstrap'
 import './styles.css'
 export default function Header(){
     return (
-        <Navbar sticky="top"  bg="light" collapseOnSelect expand="lg" className="menu">
+        <Navbar  bg="light" collapseOnSelect expand="lg" className="menu">
             <Navbar.Brand href="/" className="brand">Gerson Vaz <strong>Fotografia</strong></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
