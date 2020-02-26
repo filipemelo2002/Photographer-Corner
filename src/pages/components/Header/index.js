0d72932b285cfe5827b51d4a63db946b7e7eb502
mrnobody2002 <filipemelo032@gmail.com>
2020-02-26 14:38:07 -0300

    Router links set

--- src/pages/components/Header/index.js
+++ src/pages/components/Header/index.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
-
+import {Navbar, Nav, Form, Button} from 'react-bootstrap'
+import {NavLink} from 'react-router-dom'
 import './styles.css'
-export default function Header(){
+ function Header(){
     return (
         <Navbar  bg="light" collapseOnSelect expand="lg" className="menu">
             <Navbar.Brand href="/" className="brand">Gerson Vaz <strong>Fotografia</strong></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/ensaios">Ensaios Externos</Nav.Link>
-                    <Nav.Link href="/eventos">Outros Eventos</Nav.Link>
-                    <Nav.Link href="/wallpapers">Wallpapers</Nav.Link>
+                    <Nav.Link><NavLink to="/ensaios" className="d-inline p-2 text-black-50">Ensaios Externos</NavLink></Nav.Link>
+                    <Nav.Link><NavLink to="/eventos" className="d-inline p-2 text-black-50">Outros eventos</NavLink></Nav.Link>
+                    <Nav.Link><NavLink to="/wallpapers" className="d-inline p-2 text-black-50">Wallpapers</NavLink></Nav.Link>
                 </Nav>
-                
+                <Form inline>
+                    <Button variant="dark" type='submit'><NavLink to="/admin" className="d-inline p-2 text-white" style={{textDecoration:'none'}}>Administrador</NavLink></Button>
+                </Form>
             </Navbar.Collapse>
         </Navbar>
     )
 }
+export default Header
