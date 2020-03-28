3f3e78753557591a5a0f2cc488ee4bf785790f7d
mrnobody2002 <filipemelo032@gmail.com>
2020-03-28 16:27:25 -0300

    Refactored Ensaios Component

--- src/pages/ensaios/index.js
+++ src/pages/ensaios/index.js
@@ -1,24 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 
 import Spinner from "react-bootstrap/Spinner";
 import GalleryList from "../components/GalleryList";
-
+import FullScreenImager from "../components/FullScreenImager";
+import api from "../../service/api";
 import "./styles.css";
 
 export default function Ensaios() {
-    const [display, setDisplay] = useState("block");
+  const [toggleFullscreen, setToggleFullscreen] = useState(false);
+  const [pictures, setPictures] = useState([]);
+  const [clickedImage, setClickedImage] = useState("");
+  const display = useMemo(() => (pictures.length > 0 ? "none" : "flex"), [
+    pictures
+  ]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await api.get("/pictures/ensaios");
+      setPictures(response.data);
+      console.log(response.data);
+    }
+    fetchData();
+  }, []);
 
-    return (
-        <>
-            <div className="galeryContent">
-                <h1>Ensaios</h1>
-                <Spinner
-                    animation="grow"
-                    className="loadingIndicator"
-                    style={{ display: display }}
-                />
-                <GalleryList category="ensaios" spinner={setDisplay} />
-            </div>
-        </>
+  function myOnClick(pic) {
+    setToggleFullscreen(!toggleFullscreen);
+    setClickedImage(
+      `https://photographercorner-api.herokuapp.com${pic.picture_url}`
     );
+  }
+  return (
+    <>
+      <div className="galeryContent">
+        <h1>Ensaios</h1>
+        <Spinner
+          animation="grow"
+          className="loadingIndicator"
+          style={{ display: display }}
+        />
+        <GalleryList pictures={pictures} fullScreenClick={myOnClick} />
+        <FullScreenImager
+          image={clickedImage}
+          toggle={toggleFullscreen}
+          setToggle={setToggleFullscreen}
+        />
+      </div>
+    </>
+  );
 }
