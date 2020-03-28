23d493a3a517c6966c5a17ebebf018839f662a4f
mrnobody2002 <filipemelo032@gmail.com>
2020-03-28 16:23:34 -0300

    Directory changed

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://photographercorner-api.herokuapp.com"
+  baseURL: "https://photographercorner-api.herokuapp.com"
 });
 
 export default api;
