6ac906599d9ce9816f61fc7fb6cf7cf5c9a21f71
mrnobody2002 <filipemelo032@gmail.com>
2020-03-30 17:59:38 -0300

    Image Uploader Working Properly

--- src/pages/components/ImageUploader/index.js
+++ src/pages/components/ImageUploader/index.js
@@ -1,25 +1,44 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useContext } from "react";
 
 import api from "../../../service/api";
-import { FaFolder, FaUpload } from "react-icons/fa";
+import { FaFolder } from "react-icons/fa";
 import "./styles.css";
-
+import PictureContext from "../../context";
 export default function ImageUploader() {
+  const { addNewPicture, category: categoria } = useContext(PictureContext);
   const [image, setImage] = useState(null);
   const preview = useMemo(() => (image ? URL.createObjectURL(image) : null), [
     image
   ]);
+  async function uploadImage(e) {
+    e.preventDefault();
+    if (!image) alert("Nenhum arquivo de imagem foi selecionado");
+    const data = new FormData();
+    data.append("picture", image);
+    data.append("category", categoria);
+    const response = await api.post("/pictures", data);
+    const { id, picture_id, picture, category, picture_url } = response.data[0];
+    const newImage = {
+      id,
+      picture,
+      category,
+      picture_id,
+      picture_url
+    };
+    setImage(null);
+    addNewPicture(newImage);
+  }
   return (
     <div className="addPicture">
-      <img src={preview} width="350px" className="previewImage" />
+      <img src={preview} width="350px" className="previewImage" alt="preview" />
       <div>
         <form>
           <input
             type="file"
             onChange={event => setImage(event.target.files[0])}
           />
-          <button type="submit">
-            <FaUpload size={50} color="#3d3d3d" />
+          <button type="submit" onClick={uploadImage}>
+            <FaFolder size={50} color="#3d3d3d" />
           </button>
         </form>
       </div>
