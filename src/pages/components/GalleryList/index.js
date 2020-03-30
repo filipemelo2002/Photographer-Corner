bb3e51dc0b54f3676044f8188e59287814783dc6
mrnobody2002 <filipemelo032@gmail.com>
2020-03-30 17:55:24 -0300

    Admin actuibs ti dekete

--- src/pages/components/GalleryList/index.js
+++ src/pages/components/GalleryList/index.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./styles.css";
 import watermark from "./watermark.png";
+import { FaTrash, FaExpand } from "react-icons/fa";
+import PictureContext from "../../context";
 import ImageUploader from "../ImageUploader";
 export default function GalleryList({ pictures, fullScreenClick }) {
   const isAdmin = localStorage.getItem("adminId");
+  const { deletePicture } = useContext(PictureContext);
   return (
     <div className="pictureGrid">
-      {pictures.map((pic, id) => (
-        <div key={id} onClick={() => fullScreenClick(pic)}>
+      {pictures.map(pic => (
+        <div key={pic.picture_id} className="imageFrame">
+          <div className="deleteButton">
+            <button onClick={() => fullScreenClick(pic)}>
+              <FaExpand size={35} color="#ffffff" />
+            </button>
+            {isAdmin && (
+              <button onClick={() => deletePicture(pic)}>
+                <FaTrash size={35} color="#ffffff" />
+              </button>
+            )}
+          </div>
+
           <img
             src={watermark}
             width="350px"
@@ -16,14 +30,14 @@ export default function GalleryList({ pictures, fullScreenClick }) {
             className="watermark"
           />
           <img
-            src={`https://photographercorner-api.herokuapp.com${pic.picture_url}`}
+            src={`http://localhost:3100${pic.picture_url}`}
             width="350px"
             alt="loaded"
             className="image"
           />
         </div>
       ))}
-      {isAdmin ? <ImageUploader /> : ""}
+      {isAdmin && <ImageUploader />}
     </div>
   );
 }
