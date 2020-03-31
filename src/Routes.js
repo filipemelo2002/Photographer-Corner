a8c255959a5eab378c3ed080ad8ae74679508ef0
mrnobody2002 <filipemelo032@gmail.com>
2020-03-31 12:16:19 -0300

    Providing Header Context

--- src/Routes.js
+++ src/Routes.js
@@ -1,27 +1,40 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Home from './pages/home'
-import Ensaios from './pages/ensaios'
-import Eventos from './pages/eventos'
-import Wallpapers from './pages/wallpapers'
-import Admin from './pages/admin'
+import Home from "./pages/home";
+import Ensaios from "./pages/ensaios";
+import Eventos from "./pages/eventos";
+import Wallpapers from "./pages/wallpapers";
+import Admin from "./pages/admin";
 
-import Header from './pages/components/Header'
-import Contact from './pages/components/Contact'
-export default function Routes (){
-    return (
-        <BrowserRouter>
-
-            <Header />
-            <Switch>
-                <Route exact path="/" component={Home}/> 
-                <Route path="/ensaios" component={Ensaios}/> 
-                <Route path="/eventos" component={Eventos}/>
-                <Route path="/wallpapers" component={Wallpapers}/> 
-                <Route path="/admin" component={Admin}/>
-            </Switch>
-            <Contact />
-        </BrowserRouter>
-    )
+import Header from "./pages/components/Header";
+import Contact from "./pages/components/Contact";
+import HeaderContext from "./pages/context";
+export default function Routes() {
+  const [name, setName] = useState("Administrador");
+  const [buttonType, setButtonType] = useState("outline-dark");
+  function buttonStyles() {
+    if (localStorage.getItem("adminId")) {
+      setName("Sair");
+      setButtonType("outline-danger");
+    } else {
+      setName("Administrador");
+      setButtonType("outline-dark");
+    }
+  }
+  return (
+    <BrowserRouter>
+      <HeaderContext.Provider value={{ buttonStyles }}>
+        <Header btnText={name} btnStyles={buttonType} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/ensaios" component={Ensaios} />
+          <Route path="/eventos" component={Eventos} />
+          <Route path="/wallpapers" component={Wallpapers} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
+        <Contact />
+      </HeaderContext.Provider>
+    </BrowserRouter>
+  );
 }
