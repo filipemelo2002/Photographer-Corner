3d124e967eea9910c60fbf959ed57afdea62d774
mrnobody2002 <filipemelo032@gmail.com>
2020-04-01 21:19:03 -0300

    DND Provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Routes from "./Routes";
 import "./App.css";
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Routes />
-    </>
+    </DndProvider>
   );
 }
 
