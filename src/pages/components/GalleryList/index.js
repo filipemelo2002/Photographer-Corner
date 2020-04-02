ece70e5e11f8963cd534bf83bf0f989ef3179d52
mrnobody2002 <filipemelo032@gmail.com>
2020-04-01 21:19:32 -0300

    Refactored Gallery List

--- src/pages/components/GalleryList/index.js
+++ src/pages/components/GalleryList/index.js
@@ -1,41 +1,20 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import "./styles.css";
-import watermark from "./watermark.png";
-import { FaTrash, FaExpand } from "react-icons/fa";
-import PictureContext from "../../context";
+import Picture from "./Picture";
 import ImageUploader from "../ImageUploader";
 export default function GalleryList({ pictures, fullScreenClick }) {
   const isAdmin = localStorage.getItem("adminId");
-  const { deletePicture } = useContext(PictureContext);
   return (
     <div className="pictureGrid">
-      {pictures.map(pic => (
-        <div key={pic.picture_id} className="imageFrame">
-          <div className="deleteButton">
-            <button onClick={() => fullScreenClick(pic)}>
-              <FaExpand size={35} color="#ffffff" />
-            </button>
-            {isAdmin && (
-              <button onClick={() => deletePicture(pic)}>
-                <FaTrash size={35} color="#ffffff" />
-              </button>
-            )}
-          </div>
-
-          <img
-            src={watermark}
-            width="350px"
-            alt="watermark"
-            className="watermark"
-          />
-          <img
-            src={`https://photographercorner-api.herokuapp.com${pic.picture_url}`}
-            width="350px"
-            alt="loaded"
-            className="image"
-          />
-        </div>
+      {pictures.map((pic, index) => (
+        <Picture
+          key={pic.picture_id}
+          pic={pic}
+          index={index}
+          admin={isAdmin}
+          fullScreenClick={fullScreenClick}
+        />
       ))}
       {isAdmin && <ImageUploader />}
     </div>
